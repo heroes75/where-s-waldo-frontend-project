d26222d599c143e19f74722016c0c0bb901bc6f2
heroes75 <123456789+odin@users.noreply.github.com>
2026-05-02 04:09:40 +0000

    trying to making perfect Image function but nothing new

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -3,82 +3,73 @@ import styles from "../Styles/styleImage.module.css";
 
 export default function Image({ targets, imgUrl, unknownFunction }) {
     const [scale, setScale] = useState(1);
-    const [lastScrollTop, setLastScrollTop] = useState(document.documentElement.scrollTop);
+    const [cursorPosition, setCursorPosition] = useState({x:0, y:0});
+    const [prevCursorPosition, setPrevCursorPosition] = useState({x:0, y:0});
+    const [clientPosition, setClientPosition] = useState({x:0, y:0});
+    const [onMove, setOnMove] = useState(false);
     // const [pageYOffset, setPageYOffset] = useState(0);
     
 
     // useEffect(() => {
-    //     window.addEventListener("scroll", handleZoom);
-    //     return () => window.removeEventListener("scroll", handleZoom);
-    // });
+    //     if (isCursorOnImage) {
+    //         console.log('isCursorOnImage:', isCursorOnImage)
+    //         window.addEventListener("wheel", handleZoom, false);
+    //     } else {
+    //     return () => window.removeEventListener("wheel", handleZoom, false);
+
+    //     }
+    // }, [isCursorOnImage]);
+
     function HandlePosition(e) {
-        // console.log('e:', e)
         const rect = e.target.getBoundingClientRect();
-        // console.log("rect.x:", rect.x, "rect.y", rect.y);
-        // console.log("rect.height:", rect.height, "rect.width", rect.width);
-        // console.log("rect.top:", rect.top, "rect.left", rect.left);
-        // console.log("rect.right:", rect.right, "rect.bottom", rect.bottom);
-        // console.log("e.clientX", e.clientX, "e.clientY", e.clientY);
-        // console.log(
-        //     " e.clientX- rect.x",
-        //     e.clientX - Math.floor(rect.x),
-        //     " e.clientY- rect.y",
-        //     e.clientY - Math.floor(rect.y),
-        // );
+        console.log('rect.top:', rect.top)
+        console.log('rect.height:', rect.height)
+        console.log('rect.x:', rect.x)
+        console.log('rect.y:', rect.y)
+        console.log('e.clientX:', e.clientX)
+        console.log('e.clientY:', e.clientY)
+        const x = (e.clientX - Math.floor(rect.x))
+        const y = (e.clientY - Math.floor(rect.y))
+        console.log('{x, y}:', {x, y})
+        setClientPosition({x: e.clientX, y: e.clientY})
+        setCursorPosition({x, y})
+        setOnMove(true)
         // console.log("e.pageX", e.pageX, "e.pageY", e.pageY);
     }
 
-    function handleZoom() {
-        // console.log('window.pageYOffset:', window.pageYOffset)
-        // console.log('lastScrollTop:', lastScrollTop)
-        const currentScroll = document.documentElement.scrollTop 
-
-        if (currentScroll > lastScrollTop) {
-            console.log('Down')
-        } else {
-            console.log('Up')
-        }
-        setLastScrollTop(document.documentElement.scrollTop)
-        // let st = window.pageYOffset || document.documentElement.scrollTop;
-        // if (st > lastScrollTop) {
-        //     console.log("defilement vers le bas");
-        //     setScale(prev => prev - .005)
-        // } else if (st < lastScrollTop) {
-        //     setScale(prev => prev + .005)
-        //     console.log("defilement vers le haut");
-        // }
-
-        // setLastScrollTop(st <= 0 ? 0 : st);
+    function handleZoom(e) {
+        // e.preventDefault()
+        setOnMove(false)
+        console.log('e.deltaY:', e.deltaY)
+        console.log('scale', scale)
+        console.log('cursorPosition', cursorPosition)
+        setPrevCursorPosition({x: cursorPosition.x, y: cursorPosition.y})
+        setScale(prev => Math.min(Math.max(prev + e.deltaY * -0.01, .125), 8))
     }
-    function addZoom() {
-        window.addEventListener("scroll", handleZoom, false);
-    }
-
-    function deleteZoom() {
-        window.removeEventListener("scroll", handleZoom, false);
-        console.log('done')
-    }
-
-    // handleZoom()
+    
     return (
         <>
-            <div className={styles.imgContainer} style={{ padding: "30px" }}>
+            <span style={{position: 'relative', left: `${clientPosition.x}px`, top: `${cursorPosition.y}px`, zIndex: 2}}>{cursorPosition.x}:{cursorPosition.y}</span>
+            <div className={styles.imgContainer} style={{ padding: "0", position: 'relative', margin:0}}>
                 <img
                     className={styles.img}
-                    style={{
+                    style={!onMove ? {
                         margin: 0,
                         padding: 0,
                         width: "1000px",
                         transform: `scale(${scale})`,
+                        transformOrigin: `${cursorPosition.x}px ${cursorPosition.y}px`
+                    } : {
+                        margin: 0,
+                        padding: 0,
+                        width: "1000px",
+                        transform: `scale(${scale})`,
+                        transformOrigin: `${prevCursorPosition.x}px ${prevCursorPosition.y}px`
                     }}
                     onMouseMove={HandlePosition}
                     src="src/assets/level2-scene.webp"
                     alt="image of level 2"
-                    // onPointerEnter={addZoom}
-                    // onPointerLeave={deleteZoom}
-                    onMouseEnter={addZoom}
-                    onMouseLeave={deleteZoom}
-                    // onScroll={handleZoom}
+                    onWheel={handleZoom}
                 />
             </div>
         </>
