a015dd8bc832b5e8ea88150da84d088eade1ea45
heroes75 <123456789+odin@users.noreply.github.com>
2026-06-11 03:10:29 +0000

    modify socket.js config thrice

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,18 @@
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
-    autoConnect: false
-})
-
-export const socketMultiplayer = io(import.meta.env.VITE_API_URL + '/multiplayer', {
     autoConnect: false,
-    transports: ['websocket'],
-    upgrade: false,
-    forceNew: true, 
-})
+});
 
-export default socket
+export const socketMultiplayer = io(
+    import.meta.env.VITE_API_URL + "/multiplayer",
+    {
+        autoConnect: false,
+        reconnection: true,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        reconnectionAttempts: 5,
+    },
+);
+
+export default socket;
